9705e18e362826df9492574a613cdd95c7a7efc0
aUxbl4 <49368654+aUxbl4@users.noreply.github.com>
2019-09-02 13:12:40 +0500

    delete_old_traces.sh
    Search and delete files in a directory (template)
    Paramatras:
    
    Search the directory (by mask) and delete files
    Number of days to search and delete
    Older than 7 days (every hour):
    0 * * * * /home/oracle/dba/crontab/delete_old_traces.sh / xoracle / app / oracle / product / base11204 7> / dev / null 2> & 1

---
+++ crontab_tash_sh/delete_old_trace.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/sh
+ 
+if [ $# -lt 2 ]; then
+echo "#################################################################################"
+echo "#                                                                               #"
+echo "#    Usage:   ./delete_old_traces.sh <RDBMS_DIR> <DEPTH>                        #"
+echo "#    Example: ./delete_old_traces.sh /oracle/app/oracle/product/base11204 14    #"
+echo "#                                                                               #"
+echo "#################################################################################"
+exit 1;
+ 
+else
+
+RDBMS_DIR=$1
+DEPTH=$2
+
+find ${RDBMS_DIR} -name *.aud -mtime +${DEPTH} | xargs rm > /dev/null 2>&1
+find ${RDBMS_DIR} -name *.trc -mtime +${DEPTH} | xargs rm > /dev/null 2>&1
+find ${RDBMS_DIR} -name *.trm -mtime +${DEPTH} | xargs rm > /dev/null 2>&1
+find ${RDBMS_DIR} -name core_* -type d -mtime +${DEPTH} | xargs rm -rf > /dev/null 2>&1
+find ${RDBMS_DIR} -name log_*.xml -mtime +${DEPTH} | xargs rm > /dev/null 2>&1
+find ${RDBMS_DIR} -name log_*.xml -mtime +${DEPTH} | grep "diag/tnslsnr" | xargs rm > /dev/null 2>&1
+
+fi
