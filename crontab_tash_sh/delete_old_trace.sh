74166110c4f9e48b3abd8594bc091a1f748982d7
aUxbl4 <49368654+aUxbl4@users.noreply.github.com>
2019-10-04 10:41:41 +0500

    Update delete_old_trace.sh

--- crontab_tash_sh/delete_old_trace.sh
+++ crontab_tash_sh/delete_old_trace.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/sh
  
 if [ $# -lt 2 ]; then
-echo "#################################################################################"
-echo "#                                                                               #"
-echo "#    Usage:   ./delete_old_traces.sh <RDBMS_DIR> <DEPTH>                        #"
-echo "#    Example: ./delete_old_traces.sh /oracle/app/oracle/product/base11204 14    #"
-echo "#                                                                               #"
-echo "#################################################################################"
-exit 1;
+   echo "#################################################################################"
+   echo "#                                                                               #"
+   echo "#    Usage:   ./delete_old_traces.sh <RDBMS_DIR> <DEPTH>                        #"
+   echo "#    Example: ./delete_old_traces.sh /oracle/app/oracle/product/base11204 14    #"
+   echo "#                                                                               #"
+   echo "#################################################################################"
+   exit 1;
 else
 
 RDBMS_DIR=$1
